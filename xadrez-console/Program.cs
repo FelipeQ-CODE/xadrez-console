928725e7555dd3436594db0242407d0c17ea8fb3
Felipe Quirino <Felipe.wi23.fq@Gmail.com>
2025-01-08 09:46:54 -0300

    Projeto criado

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using Tabuleiro;
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main (string[] args) {
+
+             Posicao P;
+             P = new Posicao(3, 4);
+             Console.WriteLine("Posicao: " + P.linha + ", " + P.coluna);
+              
+        }
+    }
+}
